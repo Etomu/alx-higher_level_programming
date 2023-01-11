6e8bc5b154383d5b7a450a3dc00a3a58f4022416
tobi <113621482+Etomu@users.noreply.github.com>
2023-01-11 22:43:48 +0100

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,5 @@
-earch_replace(my_list, search, replace):
+#!/usr/bin/python3
 
+
+def search_replace(my_list, search, replace):
     return list(map(lambda e: replace if e == search else e, my_list))
